cd164227fcab23ff8911e1a6dc99d727250c781c
Praveen Kumar Bisht <praveen.jngec@gmail.com>
2021-06-05 11:49:05 +0530

    adde multi file

---
+++ multiply.java
@@ -0,0 +1,10 @@
+public class multiply {
+    
+    public static void main(String [] args){
+
+        int a=0;
+        int b=9;
+        int c = a*b;
+        System.out.println("now start commintin in thi sum"+c);
+    }
+}
